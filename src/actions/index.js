2a2237d79b2ab143620c83b1aa12207695e82a1b
Kirill Skobkarev <kirix@inbox.ru>
2017-03-09 18:14:48 +0300

    Deleted debugger directives

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,9 +13,7 @@ export const addMiss = (x, y) => ({
 })
 
 export const checkField = (x, y) => (dispatch, getState) => {
-    debugger
     battleShipService.checkField(x, y).then(action => {
-        debugger
         switch (action.type) {
             case cellState.hit: return dispatch(addHit(x, y, action.newSunkenShip));
             case cellState.miss: return dispatch(addMiss(x, y));
