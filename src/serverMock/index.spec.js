6158db2de8b5abcef544b3c80f39692222907772
Kirill Skobkarev <kirix@inbox.ru>
2017-03-09 19:59:15 +0300

    Fixed tests

--- src/serverMock/index.spec.js
+++ src/serverMock/index.spec.js
@@ -1,22 +1,23 @@
 import ServerSide from './index'
+import { cellState, boardSize } from "../constants"
 
 describe('server mock tests', () => {
     it('should return miss', () => {
         const serverSide = new ServerSide();
         const hitResult = serverSide.hitField(1, 1);
-        expect(hitResult).toEqual({ type: "miss" });
+        expect(hitResult).toEqual({ type: cellState.miss });
     })
     it('should return hit', () => {
         const serverSide = new ServerSide();
         const hitResult = serverSide.hitField(2, 9);
-        expect(hitResult).toEqual({ type: "hit" });
+        expect(hitResult).toEqual({ type: cellState.hit });
     })
     it('should return hit and sunk', () => {
         const serverSide = new ServerSide();
         const hitResult = serverSide.hitField(0, 0);
-        expect(hitResult).toEqual({ type: "hit" });
+        expect(hitResult).toEqual({ type: cellState.hit });
         const secondHitResult = serverSide.hitField(1, 0);
-        expect(secondHitResult).toEqual({ type: "hit", newSunkenShip: "destroyer" });
+        expect(secondHitResult).toEqual({ type: cellState.hit, newSunkenShip: "destroyer" });
     })
     it('should return already hit', () => {
         const serverSide = new ServerSide();
